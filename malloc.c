4ad42288a42afa9033aee89d136a7ea715d960f5
mina86 <mina86>
2005-08-18 01:09:14 +0000

    + Signal handler added

--- malloc.c
+++ malloc.c
@@ -1,6 +1,6 @@
 /*
  * Allocates specified amount of memory.
- * $Id: malloc.c,v 1.4 2005/07/11 00:20:58 mina86 Exp $
+ * $Id: malloc.c,v 1.5 2005/08/18 01:09:14 mina86 Exp $
  * Copyright (c) 2005 by Michal Nazareicz (mina86/AT/tlen.pl)
  * Licensed under the Academic Free License version 2.1.
  */
@@ -11,8 +11,16 @@
  * slow however it is not certain and many would disagree.
  */
 
+/* Comment the next line out if you don't compile for platform with
+   signal.h file. */
+#define HAVE_SIGNAL_H
+
+
 #include <stdlib.h>
 #include <stdio.h>
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
 
 
 /**** Prints size in friendly way ****/
@@ -35,6 +43,15 @@ void print_size(int size) {
 }
 
 
+#ifdef HAVE_SIGNAL_H
+/**** Signal handler ****/
+int signum = 0;
+void signal_handler(int sig) {
+	signum = sig;
+}
+#endif
+
+
 /**** Main ****/
 int main(int argc, char **argv) {
 	long to_allocate, allocated = 0;
@@ -42,17 +59,26 @@ int main(int argc, char **argv) {
 
 	/* Parse arguments */
 	to_allocate =  argc!=2 || !*argv[1] ? -1 : strtol(argv[1], argv + 1, 0);
-	if (to_allocate<0 || argv[1]) {
+	if (to_allocate<0 || *argv[1]) {
 		puts("usage: malloc <number>");
 		puts("where <number> is number of KiBs to allocate");
-		return 1;
+		return 2;
 	}
+
+#ifdef HAVE_SIGNAL_H
+	/* Catch signals */
+	for (dot = 0; dot<32; signal(++dot, &signal_handler));
+#endif
 
 	/* Allocate */
 	dot = to_allocate>2048 ? to_allocate>>11 : 1;
 	setvbuf(stdout, NULL, _IONBF, 0);
 
+#ifdef HAVE_SIGNAL_H
+	while (!signum && allocated<to_allocate && calloc(dot, 1024)!=NULL) {
+#else
 	while (allocated<to_allocate && calloc(dot, 1024)!=NULL) {
+#endif
 		putchar('.');
 		allocated += dot;
 		if ((d = (d+1) & 63)==0) {
@@ -68,5 +94,9 @@ int main(int argc, char **argv) {
 	if (d) {
 		print_size(allocated);
 	}
+#ifdef HAVE_SIGNAL_H
+	return signum ? -signum : (allocated < to_allocate ? 1 : 0);
+#else
 	return allocated < to_allocate ? 1 : 0;
+#endif
 }
