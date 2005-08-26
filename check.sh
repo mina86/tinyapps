e306733d4b06c32b3d9bc156ba58514495ac84ee
mina86 <mina86>
2005-08-26 16:17:52 +0000

    + License changed to GNU GPL

--- check.sh
+++ check.sh
@@ -1,9 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 ##
 ## Pings specified host and runs specified program if no responding
-## $Id: check.sh,v 1.2 2005/08/11 21:35:18 mina86 Exp $
-## Copyright (c) 2005 by Michal Nazareicz (mina86/AT/tlen.pl)
-## Licensed under the Academic Free License version 2.1.
+## $Id: check.sh,v 1.3 2005/08/26 16:17:52 mina86 Exp $
+## Copyright (c) 2005 by Michal Nazarewicz (mina86/AT/tlen.pl)
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ##
 
 ##
@@ -18,7 +31,7 @@
 ##
 version () {
 	echo 'Check v0.1  (c) 2005 by Micha³ Nazarewicz'
-	echo '$Id: check.sh,v 1.2 2005/08/11 21:35:18 mina86 Exp $'
+	echo '$Id: check.sh,v 1.3 2005/08/26 16:17:52 mina86 Exp $'
 	echo
 }
 
