b90588653dd800239e5b8a750e6db8177bd2072f
mina86 <mina86>
2005-11-20 20:32:51 +0000

    + license changed to AFL

--- xgetclass.c
+++ xgetclass.c
@@ -1,21 +1,8 @@
 /*
  * Prints window's ClassHint
- * $Id: xgetclass.c,v 1.1 2005/11/20 20:18:13 mina86 Exp $
+ * $Id: xgetclass.c,v 1.2 2005/11/20 20:32:51 mina86 Exp $
  * Copyright 2005 by Michal Nazarewicz (mina86/AT/tlen.pl)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Licensed under the Academic Free License version 2.1.
  */
 
 /*
