43d49be6c57372d386bc582720363ad9d724fa2d
mina86 <mina86>
2005-12-23 14:40:10 +0000

    + license changed

--- null.c
+++ null.c
@@ -1,8 +1,22 @@
 /*
  * Discards standard input.
- * $Id: null.c,v 1.4 2005/11/27 16:57:38 mina86 Exp $
+ * $Id: null.c,v 1.5 2005/12/23 14:40:10 mina86 Exp $
  * By Michal Nazarewicz (mina86/AT/tlen.pl)
- * Released to Public Domain
+ * Copyright (c) 2005 by Michal Nazarewicz (mina86/AT/tlen.pl)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 /*
