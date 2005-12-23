e7f4f5925f3f68d1b7847e287748bed178ce4ec6
mina86 <mina86>
2005-12-23 14:35:24 +0000

    + works like null.c

--- null.sh
+++ null.sh
@@ -1,30 +1,46 @@
 #!/bin/sh
 ##
 ## Discards standard input
-## $Id: null.sh,v 1.3 2005/09/03 13:27:05 mina86 Exp $
-## By Stanislaw Klekot (dozzie/AT/irc.pl)
-## Released to Public Domain
+## $Id: null.sh,v 1.4 2005/12/23 14:35:24 mina86 Exp $
+## Copyright (c) 2005 by Stanislaw Klekot (dozzie/AT/irc.pl)
+## Copyright (c) 2005 by Michal Nazarewicz (mina86/AT/tlen.pl)
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ##
 
 ##
-## You can run it as: `something | null.sh` in which case stdou of
-## `something` will de disgarded or as `null.sh somtehing` in which
-## case stdout and stderr will be redirected to /dev/null.
+## usage:
+##   command | null         (pipe feature)
+##   null    command        (exec feature)
+##   null -d command        (exec + daemonize feature)
+##   drun    command        (synonym of  null -d command )
+##
+## Pipe feature is used to discard stdout of a command.  Exec feature
+## discards both - stdout and stderr.  If -d is given or it's run as
+## drun (daemon run) the command will be put into background.
 ##
 
-##
-## You may also add the fallowing function to your .profile file to
-## make execution of null a bit faster (every nanosecond is important)
-##
-## null () {
-##     if [ $# -eq 0 ]
-##     then cat >/dev/null
-##     else "$@" >/dev/null 2>&1
-##     fi
-## }
-##
+if [ "X${0##*/}" = Xdrun ]; then DAEMONIZE=y; else DAEMONIZE=; fi
+if [ "$2" ] && ( [ "X$1" = X-d ] || [ "X$1" = x-D ] ); then
+	DAEMONIZE=y
+	shift
+fi
 
 if [ $# -eq 0 ]
 then exec cat  >/dev/null
+elif [ "$DAEMONIZE" ]
+then exec "$@" >/dev/null 2>&1 &
 else exec "$@" >/dev/null 2>&1
 fi
