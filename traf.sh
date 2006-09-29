98f1b89e05d858349c575bcf5afcdcfca4729b78
mina86 <mina86>
2006-09-29 18:39:57 +0000

    + display changed

--- traf.sh
+++ traf.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 ##
-## Shows TX/RX for eth0 over 1sec
-## $Id: traf.sh,v 1.6 2006/09/29 18:30:49 mina86 Exp $
+## Shows TX/RX over 1sec
+## $Id: traf.sh,v 1.7 2006/09/29 18:39:57 mina86 Exp $
 ## By Michal Nazareicz (mina86/AT/mina86.com)
 ##  & Stanislaw Klekot (dozzie/AT/irc.pl)
 ## Released to Public Domain
 ##
 
+# usage: ./traf.sh <interface>
+# <interface> can be a BRE
+
+
 set -e
 INT="${1-eth0}"
+
 
 get_traffic () {
 	TX2=0; RX2=0;
@@ -20,6 +25,7 @@ get_traffic () {
 	done </proc/net/dev
 }
 
+
 get_traffic
 
 while sleep 1; do
@@ -29,19 +35,32 @@ while sleep 1; do
 
 	TX=$(( $TX2 - $TX1 ))
 	RX=$(( $RX2 - $RX1 ))
-	PROGRESS=""
+
+	PT=; PR=;
 	I=0
-	while [ $I -lt $TX -o $I -lt $RX ]; do
-		if [ $I -gt $TX ]; then
-			PROGRESS="$PROGRESS'"
-		elif [ $I -gt $RX ]; then
-			PROGRESS="$PROGRESS."
-		else
-			PROGRESS="$PROGRESS:"
-		fi
+	while [ $I -lt 15360 ]; do
+		if [ $I -lt $TX ]; then PT="#$PT"; else PT=" $PT"; fi
+		if [ $I -lt $RX ]; then PR="$PR#"; else PR="$PR "; fi
 		I=$(( $I + 512 ))
 	done
 	if [ $TX -gt 2048 ]; then TX="$(( $TX / 1024 )) K"; fi
 	if [ $RX -gt 2048 ]; then RX="$(( $RX / 1024 )) K"; fi
-	printf "< %6s > %6s  %s\n" "$TX" "$RX" "$PROGRESS"
+#	printf "%s < %6s | %6s > %s\n" "$PT" "$TX" "$RX" "$PR"
+	printf "%6s  %s | %s  %6s\n" "$TX" "$PT" "$PR" "$RX"
+
+#	PROGRESS=""
+#	I=0
+#	while [ $I -lt $TX -o $I -lt $RX ]; do
+#		if [ $I -gt $TX ]; then
+#			PROGRESS="$PROGRESS'"
+#		elif [ $I -gt $RX ]; then
+#			PROGRESS="$PROGRESS."
+#		else
+#			PROGRESS="$PROGRESS:"
+#		fi
+#		I=$(( $I + 512 ))
+#	done
+#	if [ $TX -gt 2048 ]; then TX="$(( $TX / 1024 )) K"; fi
+#	if [ $RX -gt 2048 ]; then RX="$(( $RX / 1024 )) K"; fi
+#	printf "< %6s > %6s  %s\n" "$TX" "$RX" "$PROGRESS"
 done
