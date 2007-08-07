6e44032426c82e5675392f6f26c4629949aafe65
mina86 <mina86>
2007-08-07 12:27:50 +0000

    + accepts one argument -- password length + uses pipes instead of $(...) (new lines are not stripped now)

--- genpass.sh
+++ genpass.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ##
-## Generates a random password
-## $Id: genpass.sh,v 1.2 2006/09/28 15:06:19 mina86 Exp $
+## Generates a random password (or some other key)
+## $Id: genpass.sh,v 1.3 2007/08/07 12:27:50 mina86 Exp $
 ## Copyright (c) 2005 by Michal Nazarewicz (mina86/AT/mina86.com)
 ##
 ## This program is free software; you can redistribute it and/or modify
@@ -20,60 +20,58 @@
 ##
 
 ##
-## Runing through a pipe with cut may change number of characters, eg:
-##     genpass.sh |cut -c 1-NUM
-## where NUM is number of characters.  If you need more characters you
-## can use the fallowing code (for 128 characters):
-##     printf %s "$(genpass.sh)" "$(genpass.sh)"; echo
-## You can then cut it down, eg:
-##     printf %s "$(genpass.sh)" "$(genpass.sh)" | cut -c 1-100; echo
-## The fallowing code may be used to get NUM*64 characters:
-##     for i in `seq NUM`; do genpass.sh | xargs printf %s; echo
-## And of coruse you can cut it down to NUM2 characters:
-##     for i in `seq NUM`; do genpass.sh |xargs printf %s |cut -c1-NUM2; echo
+## You can specify number of characters password should have as
+## a parameter, eg.;
+##     ./getnpass 100
 ##
 
-ARG0="${0##*/}"
-
-##
-## Generates a password from random stream
-##
-genpass () {
-	if which uuencode >/dev/null 2>&1; then
-		PASS=`printf %s "$1" | uuencode -m pass | tail -n +2 | head -n 1`
-	elif which md5sum >/dev/null 2>&1; then
-		P1=`printf %s "${1#????????????????????????????????}" | \
-		      md5sum | cut -c1-32`
-		P2=`printf %s "${1%????????????????????????????????}" | \
-		      md5sum | cut -c1-32`
-		PASS="$P1$P2"
-	else
-		printf "%s: unable to generate random password\n" "$ARG0" >&2
-		exit 1
-	fi
-}
 
 
-##
-## Generate random password
-##
-if [ -e /dev/urandom ]; then
-	genpass "`head -c 64 /dev/urandom`"
-elif [ -e /dev/random ]; then
-	genpass "`head -c 64 /dev/random`"
-elif [ "$RANDOM" -a "$RANDOM" -ne "$RANDOM" ]; then
-	LOOP=16; PASS=
-	while [ $LOOP -gt 0 ]; do
-		PASS="$PASS`printf %04x $RANDOM`"
-		LOOP=$(( $LOOP - 1 ))
-	done
+if [ -r /dev/urandom ]; then
+	gen_stream () {
+		head -c 40 /dev/urandom
+	}
+elif [ -r /dev/random ]; then
+	gen_stream () {
+		head -c 40 /dev/random
+	}
+elif [ -n "$RANDOM" ] && [ "$RANDOM" -ne "$RANDOM" ]; then
+	gen_stream () {
+		set -- 10
+		while [ $1 -gt 0 ]; do
+			printf %04x $RANDOM
+			set -- $(( $1 - 1 ))
+		done
+	}
 else
-	printf "%s: unable to generate random password\n" "$ARG0" >&2
+	printf "%s: unable to collect random data\n" "${0##*/}" >&2
 	exit 1
 fi
 
 
-##
-## Print password
-##
-printf "%s\n" "$PASS"
+
+if which uuencode >/dev/null 2>&1; then
+	gen_pass () {
+		printf %s $(uuencode -m pass | tail -n +2 | head -n 1)
+	}
+elif which md5sum >/dev/null 2>&1; then
+	gen_pass () {
+		P1=`head -c 32 | md5sum | cut -c1-32`
+		P2=`head -c 32 | md5sum | cut -c1-32`
+		printf %s "$P1" "$P2"
+	}
+else
+	printf "%s: unable to generate random password\n" "${0##*/}" >&2
+	exit 1
+fi
+
+
+
+LEN=${1:-64}
+if [ "$LEN" -gt 0 ]; then
+	L=$LEN
+	while [ $L -gt 0 ]; do
+		gen_stream | gen_pass
+		L=$(( $L - 64 ))
+	done | cut -c 1-$LEN
+fi
