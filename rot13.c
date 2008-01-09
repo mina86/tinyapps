a2495964b3eb7c2a98dfa3fda6798fa0ec913f33
mina86 <mina86>
2008-01-09 18:46:41 +0000

    *** empty log message ***

--- rot13.c
+++ rot13.c
@@ -1,7 +1,8 @@
 /*
- * Ecrypts message using ROT13
- * $Id: rot13.c,v 1.2 2005/12/29 18:50:12 mina86 Exp $
+ * Encrypts message using ROT13
+ * $Id: rot13.c,v 1.4 2008/01/09 18:46:41 mina86 Exp $
  * Released to Public Domain
  */
 
+#include <stdio.h>
 main(a){while(a=~getchar())putchar(~a-1/(~(a|32)/13*2-11)*13);}
