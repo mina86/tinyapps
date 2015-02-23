fdf453f15a83a916f3be921643bb386b03af6a22
Michal Nazarewicz <mina86@mina86.com>
2015-02-23 23:06:29 +0100

    [cdiff] Update license to GPLv3+
    Now, the whole project uses GPLv3+.

--- cdiff.sh
+++ cdiff.sh
@@ -7,19 +7,21 @@
 ## With no arguments, the context or unified diff on stdin is colorized.
 ## Otherwise, ${DIFF:-/usr/bin/diff} is run and its output colorized.
 ##
-## Copyright (c) 2005 by Michal Nazareicz (mina86/AT/mina86.com)
+## Copyright (c) 2005 by Michal Nazareicz <mina86@mina86.com>
+## Copyright (c) 2013 by Mark Edgar <medgar123@gmail.com>
 ##
-## Licensed under the Apache License, Version 2.0 (the "License");
-## you may not use this file except in compliance with the License.
-## You may obtain a copy of the License at
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
 ##
-##     http://www.apache.org/licenses/LICENSE-2.0
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
 ##
-## Unless required by applicable law or agreed to in writing, software
-## distributed under the License is distributed on an "AS IS" BASIS,
-## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-## See the License for the specific language governing permissions and
-## limitations under the License.
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
 ##
 ## This is part of Tiny Applications Collection
 ##   -> http://tinyapps.sourceforge.net/
